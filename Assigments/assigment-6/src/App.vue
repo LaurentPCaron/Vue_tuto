63d4298b98397cc3d1c6ea183fa964d1b7e2ffe4
laurent <laurentpcaron@gmail.com>
2022-03-13 00:34:35 -0500

    Exercie

---
+++ Assigments/assigment-6/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <active-user :name="userName" :age="userAge"></active-user>
+    <user-data @get-new-user="addNewUser"></user-data>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: "",
+      userAge: "",
+    };
+  },
+  methods: {
+    addNewUser(name, age) {
+      this.userName = name;
+      this.userAge = age;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ Assigments/assigment-6/src/components/UserData.vue
@@ -0,0 +1,34 @@
+<template>
+  <form action="" @submit.prevent="onUserSubmit">
+    <label for="name-input">Name: </label>
+    <input type="text" name="name" id="name-input" v-model="nameInput" />
+    <label for="age-iunput">Age: </label>
+    <input type="number" name="age" id="age-input" v-model="ageInput" />
+    <button>New User</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nameInput: "Laurent",
+      ageInput: "31",
+    };
+  },
+
+  emits: ["get-new-user"],
+
+  beforeMount() {
+    this.$emit("get-new-user", this.nameInput, this.ageInput);
+  },
+  methods: {
+    onUserSubmit() {
+      this.$emit("get-new-user", this.nameInput, this.ageInput);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
