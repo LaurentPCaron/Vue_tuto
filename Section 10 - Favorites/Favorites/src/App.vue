072f91daa31abe8d5ce3e08f9ea87ea86dfb25d9
laurent <laurentpcaron@gmail.com>
2022-03-25 22:32:33 -0400

    Section 10 - Start

---
+++ Section 10 - Favorites/Favorites/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedRessources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The official Vue.js documentation",
+          link: "https://vuejs.org",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
