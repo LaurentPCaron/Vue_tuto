e062cd3f44879d84c5699a915e181d3f59351447
laurent <laurentpcaron@gmail.com>
2022-03-25 21:04:43 -0400

    FIn  Section 9

---
+++ Section 09 Deeper in components/Badge/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectedComponents === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponents === 'manage-goals'"></manage-goals> -->
+    <keep-alive>
+      <component :is="selectedComponents"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/layout/TheHeader.vue";
+
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
+export default {
+  components: {
+    TheHeader,
+    ManageGoals,
+    ActiveGoals,
+  },
+  data() {
+    return {
+      selectedComponents: "active-goals",
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponents = cmp;
+    },
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
